4d9de4f90753ef1e553e16f1ffb665c0f2aa2be3
minji25 <zakijeong917@naver.com>
2025-06-24 16:51:22 +0900

    ShopNPC수정

--- Assets/강다은/Scripts/ShopNPC.cs
+++ Assets/강다은/Scripts/ShopNPC.cs
@@ -1,32 +1,39 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ShopNPC : MonoBehaviour
 {
-	private void OnTriggerEnter(Collider other)
-	{
-		if (other.CompareTag("Player"))
-		{
-			Debug.Log($"[ShopNpc] {npcId} 상점 열림");
-			shopUI.Show();
-			shopUI.OpenShop(npcId);
-			bigInventoryUI.OpenOrClose(true);
-			resellButton.OpenOrCloseResellButton(true);
-		}
-	}
+    [SerializeField] private string npcId;
+    [SerializeField] private ShopUI shopUI;
+    [SerializeField] private GameObject ingameUI;
 
-	private void OnTriggerExit(Collider other)
-	{
-		if (other.CompareTag("Player"))
-		{
-			shopUI.Hide(); // UI 끄기
+    private BigInventoryUI bigInventoryUI;
+    private Resell resellButton; // 버튼용 스크립트
+
+    private void Awake()
+    {
+        bigInventoryUI = ingameUI.GetComponentInChildren<BigInventoryUI>(true);
+        resellButton = ingameUI.GetComponentInChildren<Resell>(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log($"[ShopNpc] {npcId} 상점 열림");
+            shopUI.Show();
+            shopUI.OpenShop(npcId);
+            bigInventoryUI.OpenOrClose(true);
+            resellButton.OpenOrCloseResellButton(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            shopUI.Hide();
             bigInventoryUI.OpenOrClose(false);
             resellButton.OpenOrCloseResellButton(false);
         }
-	}
-
-	[SerializeField] private string npcId;
-	[SerializeField] private ShopUI shopUI;
-	[SerializeField] private BigInventoryUI bigInventoryUI;
-	[SerializeField] private Resell resellButton;
+    }
 }
