aa0badb4ee1776245b2a865fd439da24f91c8ece
minji25 <zakijeong917@naver.com>
2025-06-29 00:28:35 +0900

    커밋

--- Assets/신원석/BasicSpawner.cs
+++ Assets/신원석/BasicSpawner.cs
@@ -37,7 +37,7 @@ public class BasicSpawner : MonoBehaviour, INetworkRunnerCallbacks
         await _runner.StartGame(new StartGameArgs()
         {
             GameMode = GameMode.AutoHostOrClient,
-            SessionName = "TestRoom12",
+            SessionName = "TestRoom1234",
             Scene = scene,
             SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
         });
--- Assets/정민지/Scripts/Hp&Stamina/PlayerStamina.cs
+++ Assets/정민지/Scripts/Hp&Stamina/PlayerStamina.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class PlayerStamina : NetworkBehaviour
 {
-    [Networked] public float currentStamina { get; set; }
+    public float currentStamina { get; set; }
 
     public float AttackStaminaCost { get; set; } = 10f;
 
