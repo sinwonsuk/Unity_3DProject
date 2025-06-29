788cf1e8139da04efcd8aa0df751f758d356b303
dusdn932 <50188283+dusdn932@users.noreply.github.com>
2025-06-29 15:34:41 +0900

    Reapply "Merge branch 'main' of https://github.com/sinwonsuk/Unity_3DProject"
    This reverts commit fa6da09495bc13628a3a997b9df9890b4fd9c262.

--- Assets/강다은/Scripts/ShopNPC.cs
+++ Assets/강다은/Scripts/ShopNPC.cs
@@ -1,57 +1,127 @@
 using Fusion;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class ShopNPC : NetworkBehaviour
 {
+    [Header("NPC 설정")]
     [SerializeField] private string npcId;
-    [SerializeField] private ShopUI shopUI;
-    //[SerializeField] private GameObject ingameUI;
 
+    // UI 참조
+    private ShopUI shopUI;
     private BigInventoryUI bigInventoryUI;
-    private Resell resellButton; // 버튼용 스크립트
-
-    //private void Awake()
-    //{
-    //    bigInventoryUI = ingameUI.GetComponentInChildren<BigInventoryUI>(true);
-    //    resellButton = ingameUI.GetComponentInChildren<Resell>(true);
-    //}
+    private Resell resellButton;
 
     public override void Spawned()
     {
-        if (Object.HasInputAuthority || Object.HasStateAuthority) // 로컬에서만
+        base.Spawned();
+        var uiGO = GameObject.Find("ShopUICanvas");
+        if (uiGO == null)
         {
-            GameObject ingameUI = GameObject.Find("InGameUI"); // 씬 안의 오브젝트
-            if (ingameUI != null)
-                LinkUI(ingameUI);
+            Debug.LogError("[ShopNPC] ShopUICanvas를 찾지 못했습니다!");
         }
-    }
+        else
+        {
+            Debug.Log("[ShopNPC] ShopUICanvas를 찾았다잉~");
+            shopUI = uiGO.GetComponent<ShopUI>();
+            // 초기에는 숨겨 두기
+            uiGO.SetActive(false);
+        }
 
-    public void LinkUI(GameObject ingameUI)
-    {
-        shopUI = ingameUI.GetComponentInChildren<ShopUI>(true);
-        bigInventoryUI = ingameUI.GetComponentInChildren<BigInventoryUI>(true);
-        resellButton = ingameUI.GetComponentInChildren<Resell>(true);
+        // 씬에 배치된 InGameUI 에서 BigInventoryUI, Resell 버튼 찾기
+        var ig = GameObject.Find("InGameUI");
+        if (ig == null)
+        {
+            Debug.LogError("[ShopNPC] InGameUI를 찾지 못했습니다!");
+        }
+        else
+        {
+            bigInventoryUI = ig.GetComponentInChildren<BigInventoryUI>(true);
+            resellButton = ig.GetComponentInChildren<Resell>(true);
+
+            if (bigInventoryUI == null) Debug.LogError("[ShopNPC] BigInventoryUI가 없습니다!");
+            if (resellButton == null) Debug.LogError("[ShopNPC] Resell 버튼이 없습니다!");
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        var root = other.transform.root.gameObject;
+        if (!root.CompareTag("Player"))
+            return;
+
+        // NetworkObject 검색
+        var pobj = other.GetComponentInParent<NetworkObject>();
+        if (pobj == null)
+            return;
+        if (pobj.HasInputAuthority)
         {
-            Debug.Log($"[ShopNpc] {npcId} 상점 열림");
-            shopUI.Show();
-            shopUI.OpenShop(npcId);
-            bigInventoryUI.OpenOrClose(true);
-            resellButton.OpenOrCloseResellButton(true);
+            if (shopUI != null)
+            {
+                shopUI.Show();
+                shopUI.OpenShop(npcId);
+            }
+            if (bigInventoryUI != null)
+                bigInventoryUI.OpenOrClose(true);
+            if (resellButton != null)
+                resellButton.OpenOrCloseResellButton(true);
         }
+
+        if (Runner.IsServer)
+            Rpc_OpenShop(pobj.InputAuthority, npcId);
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player"))
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        var root = other.transform.root.gameObject;
+        if (!root.CompareTag("Player"))
+            return;
+
+        var pobj = other.GetComponentInParent<NetworkObject>();
+        if (pobj == null)
+            return;
+
+        // 로컬 플레이어인 경우 즉시 UI 닫기
+        if (pobj.HasInputAuthority)
         {
-            shopUI.Hide();
-            bigInventoryUI.OpenOrClose(false);
-            resellButton.OpenOrCloseResellButton(false);
+            if (shopUI != null) shopUI.Hide();
+            if (bigInventoryUI != null) bigInventoryUI.OpenOrClose(false);
+            if (resellButton != null) resellButton.OpenOrCloseResellButton(false);
         }
+
+        // 서버인 경우 RPC로도 다른 쪽 알림
+        if (Runner.IsServer)
+            Rpc_CloseShop(pobj.InputAuthority);
+    }
+
+    [Rpc(RpcSources.StateAuthority, RpcTargets.InputAuthority)]
+    private void Rpc_OpenShop(PlayerRef target, string npcId)
+    {
+        // 이 코드는 오직 해당 플레이어 클라이언트에서 실행됩니다
+        if (shopUI != null)
+            shopUI.Show();
+        if (bigInventoryUI != null)
+            bigInventoryUI.OpenOrClose(true);
+        if (resellButton != null)
+            resellButton.OpenOrCloseResellButton(true);
+
+        // (필요하다면 OpenShop RPC로도 호출할 수 있습니다)
+        shopUI?.OpenShop(npcId);
+    }
+
+    [Rpc(RpcSources.StateAuthority, RpcTargets.InputAuthority)]
+    private void Rpc_CloseShop(PlayerRef target)
+    {
+        if (shopUI != null)
+            shopUI.Hide();
+        if (bigInventoryUI != null)
+            bigInventoryUI.OpenOrClose(false);
+        if (resellButton != null)
+            resellButton.OpenOrCloseResellButton(false);
     }
 }
